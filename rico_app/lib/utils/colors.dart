361da50ad7d6582e07ab41cba69217f46e052ae6
JonathanDominguez-jd <2529252020@mail.utec.edu.sv>
2024-09-17 11:44:16 -0600

    Creamos la clase que contiene los colores

---
+++ rico_app/lib/utils/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //Colores de fondo
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color buttonBackGroundColor = Color(0xFFFF8B24);
+  static const Color textFieldBackgroundColor = Color(0xFFEEEEEE);
+  static const Color cardBackgroundColor = Color(0xFFD9D9D9);
+  static const Color appBarBackgroundColor = Color(0xFFFF8B24);
+  static const Color bottomNavigationBarColor = Color(0xFFFF8B24);
+
+  //Colores de texto
+  static const Color primaryTextColor = Color(0xFF000000);
+  static const Color secondTextColor = Color(0xFFFFFFFF);
+  static const Color titleTextColor = Color(0xFFFF8B24);
+  static const Color subTitleTextColor = Color(0xFF686D76);
+  static const Color hintTextFieldTextColor = Color(0xFF686D76);
+  static const Color textFieldTextColor = Color(0xFFFF8B24);
+  static const Color buttonTextColor = Color(0xFFFFFFFF);
+  static const Color descriptionTextColor = Color(0xFF686D76);
+
+  //Colores de bordes
+  static const Color primaryBorderColor = Color(0xFFFF8B24);
+  static const Color secondBorderColor = Color(0xFF807C7C);
+
+  //Colores de iconos
+  static const Color primaryIconColor = Color(0xFF000000);
+  static const Color secondIconColor = Color(0xFFFFFFFF);
+  static const Color warningColor = Color(0xFFDA0606);
+}
